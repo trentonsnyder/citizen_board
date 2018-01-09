0cda98243f29c1cf69c17e419c7e30b11051f1f0
trentonsnyder <trenton.m.snyder@gmail.com>
2018-01-08 18:03:04 -0700

    lanes new and create

--- app/controllers/auth/lanes_controller.rb
+++ app/controllers/auth/lanes_controller.rb
@@ -1,17 +1,39 @@
 class Auth::LanesController < Auth::AuthenticatedController
   before_action :set_organization
+  before_action :set_lane
+
+  def new
+    @lane = @pool.lanes.new
+  end
+
+  def create
+    @lane = @pool.lanes.new(lane_params)
+    if @lane.save
+      redirect_to auth_pool_path(@pool.id)
+    else
+      render 'new'
+    end
+  end
 
   def move_right
-    @pool = @organization.pools.find(params[:pool_id])
     @lane = @pool.lanes.find(params[:id])
     @lane.move_lower
     redirect_to auth_pool_path(@pool.id)
   end
 
   def move_left
-    @pool = @organization.pools.find(params[:pool_id])
     @lane = @pool.lanes.find(params[:id])
     @lane.move_higher
     redirect_to auth_pool_path(@pool.id)
+  end
+
+  private
+
+  def set_lane
+    @pool = @organization.pools.find(params[:pool_id])
+  end
+
+  def lane_params
+    params.require(:lane).permit(:name)
   end
 end
